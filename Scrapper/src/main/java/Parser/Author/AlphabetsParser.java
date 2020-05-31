131590ddde65cbe7e0f6c8a115d1f25ad06ebc4e
Yash <yashpatel26@gmail.com>
2020-05-31 13:02:20 -0400

    Finished coding AlphabetsParser.

--- Scrapper/src/main/java/Parser/Author/AlphabetsParser.java
+++ Scrapper/src/main/java/Parser/Author/AlphabetsParser.java
@@ -1,4 +1,14 @@
 package Parser.Author;
 
+import java.util.ArrayList;
+
 public class AlphabetsParser {
+    public static ArrayList<Author> parseAlphabetsForAuthors() {
+        ArrayList<Author> authors = new ArrayList<>();
+        for(char alphabet = 'a'; alphabet <='z'; alphabet++ )
+        {
+            authors.addAll(AlphabetParser.parseAlphabet(alphabet));
+        }
+        return authors;
+    }
 }
